aa8d30a97a3e0ed43cd2eae1796d6aaf860adcb3
Enver <humanenver@gmail.com>
2020-02-20 16:29:38 +0200

    Adding files to the project

---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        println("Hello World");
+
+    }
+
+    public static void println(String data)
+    {
+        System.out.println("[MAIN]"+ data);
+    }
+
+
+    public static void println(int data)
+    {
+
+        System.out.println("[MAIN]"+ data);
+    }
+
+
+
+
+}
